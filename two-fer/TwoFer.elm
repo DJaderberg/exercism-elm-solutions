2d1a2b27f565a99b1e4718e091a200f07a7cb4ef
David Jäderberg <david@jaderberg.net>
2019-04-19 12:42:08 +0200

    Second solution for TwoFer
    Seeing "you" as the default value for the name

--- two-fer/TwoFer.elm
+++ two-fer/TwoFer.elm
@@ -3,9 +3,4 @@ module TwoFer exposing (twoFer)
 
 twoFer : Maybe String -> String
 twoFer name =
-    case name of
-        Just n ->
-            "One for " ++ n ++ ", one for me."
-
-        Nothing ->
-            "One for you, one for me."
+    "One for " ++ Maybe.withDefault "you" name ++ ", one for me."
