39831e6aaa36140e85dd5e9c9e8209d5e2db9f4d
David Jäderberg <david@jaderberg.net>
2019-05-07 21:55:38 +0200

    Anagram using sorted lists of chars

--- anagram/Anagram.elm
+++ anagram/Anagram.elm
@@ -1,6 +1,6 @@
 module Anagram exposing (detect)
 
-import Dict exposing (Dict)
+import String exposing (toList, toUpper)
 
 
 detect : String -> List String -> List String
@@ -10,29 +10,8 @@ detect word candidates =
 
 anagram : String -> String -> Bool
 anagram a b =
-    anagramCaseSensitive (String.toUpper a) (String.toUpper b)
+    toUpper a /= toUpper b && upperSortedList a == upperSortedList b
 
 
-anagramCaseSensitive a b =
-    a /= b && count a == count b
-
-
-{-| Count how many times each Char occurs in a String
--}
-count : String -> Dict Char Int
-count s =
-    countHelper (String.toList s) Dict.empty
-
-
-countHelper : List Char -> Dict Char Int -> Dict Char Int
-countHelper list dict =
-    case list of
-        c :: cs ->
-            countHelper cs (Dict.update c increment dict)
-
-        [] ->
-            dict
-
-
-increment m =
-    Just (1 + Maybe.withDefault 0 m)
+upperSortedList =
+    toUpper >> toList >> List.sort
